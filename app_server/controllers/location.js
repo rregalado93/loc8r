d07c169f453a0c2cb3335f3d466efc69a03357b9
Ronald Regalado <regalado.ronald@hotmail.com>
2016-09-10 00:32:56 +0000

    commit 6th

---
+++ app_server/controllers/location.js
@@ -0,0 +1,14 @@
+/* Get 'home' page */
+module.exports.homelist = function (req, res, next) {
+    res.render('index', {title: 'Home'});
+};
+
+/*Get 'location info' page*/
+module.exports.locationInfo = function(req, res, next) {
+    res.render('index', {title: 'Location info'});
+};
+
+/*Get 'Add Review' page*/
+module.exports.addReview = function(req, res, next) {
+    res.render ('index', {title: "Add review"});
+};
